c9804807a617978021838ab60f32ba35716a4bc2
Nole35 <lazic.sr9.2020@uns.ac.rs>
2024-09-03 22:30:26 +0200

    ispravka sitnih gresaka

---
+++ auth-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO `korisnik` (`enabled`, `ime`, `jmbg`, `lozinka`, `prezime`, `uloga`, `username`)
+VALUES (1, 'Petar', '1234567890123', 'lozinka123', 'Petrović', 'STUDENT', 'petar123');
+
+INSERT INTO `korisnik` (`enabled`, `ime`, `jmbg`, `lozinka`, `prezime`, `uloga`, `username`)
+VALUES (1, 'Ana', '9876543210987', 'lozinka456', 'Anić', 'STUDENT', 'ana123');
